88c1e3cd54a2bf0af8b2df91a8272f27c3388df7
nithyainvincible <nithyashreee18@gmail.com>
2026-05-25 14:34:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information ')
+        st.subheader(fruit_chosen + ' Nutrition Information')
     st.write(ingredients_string)
 
 
