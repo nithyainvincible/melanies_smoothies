b84e20a30554d332e4adea5412ea515feee25598
nithyainvincible <nithyashreee18@gmail.com>
2026-05-25 12:51:25 +0530

    Create streamlit_app.py
    copied from my custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,71 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title("🥤 Customize Your Smoothie! 🥤")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
+
+import streamlit as st
+
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order )
+
+import streamlit as st
+
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home Phone', 'Mobile Phone'))
+
+if option == 'Email':
+    contact_info = st.text_input('Enter your email address:')
+elif option == 'Home Phone':
+    contact_info = st.text_input('Enter your home phone number:')
+else:
+    contact_info = st.text_input('Enter your mobile phone number:')
+
+st.write('You selected:', option)
+st.write('Your contact:', contact_info)
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+    )
+if ingredients_list:
+   
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    st.write(ingredients_string)
+
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
+   
+
+    #st.write(my_insert_stmt)
+    
+    
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+ 
+    my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
